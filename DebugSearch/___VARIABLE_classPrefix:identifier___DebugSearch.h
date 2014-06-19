1f618fb2fbdd25ba1a8c51ba0fb7e5d90af669ec
Moises <moises.aranas@gmail.com>
2014-06-19 19:41:54 +0200

    Initial Commit

---
+++ DebugSearch/___VARIABLE_classPrefix:identifier___DebugSearch.h
@@ -0,0 +1,13 @@
+//
+//  ___VARIABLE_classPrefix:identifier___DebugSearch.h
+//  ___VARIABLE_classPrefix:identifier___DebugSearch
+//
+//  Created by Moises Anthony Aranas on 6/19/14.
+//  Copyright (c) 2014 moises. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface ___VARIABLE_classPrefix:identifier___DebugSearch : NSObject
+
+@end
